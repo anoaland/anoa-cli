50a6f0a1b19d147455eadb9e3afcbef76017ece7
budiadiono <budiadiono@gmail.com>
2018-11-06 06:42:23 +0700

    map action to component

--- src/commands/view.ts
+++ src/commands/view.ts
@@ -11,8 +11,11 @@ module.exports = {
       createClassComponent,
       createStatelessComponent,
       storeStateList,
+      storeAppActionList,
       strings: { pascalCase, camelCase },
     } = context
+
+    await storeAppActionList()
 
     const taskCreateComponent = 'Create new component'
     const taskCreateScreen = 'Create new screen'
@@ -64,16 +67,21 @@ module.exports = {
           const withState = await prompt.confirm('Do you want to have state in your component?')
           let stateProps = undefined
           let stateMap = undefined
+          let actionProps = undefined
+          let actionMap = undefined
+          let actionImports = undefined
 
           const storeStates = await storeStateList()
           if (storeStates) {
-            const choices = {}
-            for (const k of Object.keys(storeStates)) {
-              choices[pascalCase(k) + 'State'] = Object.keys(storeStates[k]).map(o => k + '.' + o)
-            }
-
-            const withStoreState = await prompt.confirm('Do you want to map the reducer state?')
+            const withStoreState = await prompt.confirm(
+              'Do you want to map application state into props?',
+            )
             if (withStoreState) {
+              const choices = {}
+              for (const k of Object.keys(storeStates)) {
+                choices[pascalCase(k) + 'State'] = Object.keys(storeStates[k]).map(o => k + '.' + o)
+              }
+
               const { statesToMap } = await prompt.ask([
                 {
                   name: 'statesToMap',
@@ -97,11 +105,72 @@ module.exports = {
             }
           }
 
+          const storeAppActions = await storeAppActionList()
+          const importStatements = []
+
+          if (storeAppActions) {
+            const withStoreAction = await prompt.confirm(
+              'Do you want to map application action into props?',
+            )
+
+            if (withStoreAction) {
+              const choices = []
+              for (const k of Object.keys(storeAppActions)) {
+                choices.push(
+                  `${k}(${JSON.stringify(storeAppActions[k].params).replace(/\"/g, '')})`,
+                )
+              }
+
+              const { actionsToMap } = await prompt.ask([
+                {
+                  name: 'actionsToMap',
+                  type: 'checkbox',
+                  message: 'Select state(s) you want to map',
+                  radio: true,
+                  choices,
+                },
+              ])
+
+              actionProps = []
+              actionMap = []
+              actionImports = {}
+
+              for (const st of actionsToMap as string[]) {
+                const a = st.split('(')[0].trim()
+                const act = storeAppActions[a]
+                const prop = a.substr(0, a.length - 6)
+                actionProps.push(
+                  `${prop}: (${Object.keys(act.params)
+                    .map(k => `${k}: ${act.params[k]}`)
+                    .join(',')}) => void`,
+                )
+                actionMap.push(
+                  `${prop}: (${Object.keys(act.params).join(', ')}) => dispatch(${a}(${Object.keys(
+                    act.params,
+                  ).join(', ')}))`,
+                )
+
+                actionImports[act.file] = [...(actionImports[act.file] || []), a]
+              }
+
+              for (const impor of Object.keys(actionImports)) {
+                importStatements.push(
+                  `import { ${actionImports[impor].join(
+                    ',',
+                  )} } from '../../store/actions/${impor.substr(0, impor.length - 3)}'`,
+                )
+              }
+            }
+          }
+
           await createClassComponent(name, {
             withState,
             stateProps,
             stateMap,
-            withStore: !!stateProps,
+            actionProps,
+            actionMap,
+            importStatements,
+            withStore: !!stateProps || !!actionProps,
           })
           break
 
--- src/extensions/store.ts
+++ src/extensions/store.ts
@@ -1,8 +1,9 @@
 import { uniq } from 'ramda'
 import { fromPath } from 'ts-emitter'
 import { SyntaxKind, Node } from 'typescript'
+import { GluegunRunContext } from 'gluegun'
 
-module.exports = context => {
+module.exports = (context: GluegunRunContext) => {
   context.storeInit = async () => {
     const {
       init,
@@ -115,6 +116,7 @@ module.exports = context => {
       return []
     }
 
+    // @ts-ignore
     return tree.children.filter(r => r.type === 'dir')
   }
 
@@ -245,6 +247,99 @@ module.exports = context => {
       })
 
       results[name] = stateProps
+    }
+
+    return results
+  }
+
+  context.storeStateList = async () => {
+    const {
+      storeReducerObjectList,
+      strings: { camelCase },
+    } = context
+
+    // collect reducer's state files
+    const stateFiles = (await storeReducerObjectList())
+      .filter(r => r.children && r.children.length)
+      .map(r => {
+        if (r.children.find(c => c.name === 'state.ts')) {
+          return { name: camelCase(r.name), path: `src/store/reducers/${r.name}/state.ts` }
+        }
+        return undefined
+      })
+      .filter(s => s !== undefined)
+
+    if (!stateFiles) {
+      return undefined
+    }
+
+    let results = {}
+
+    for (const { path, name } of stateFiles) {
+      const ast = fromPath(path)
+
+      // let stateName = ''
+      let stateProps = {}
+
+      ast.forEachChild(c => {
+        if (c.kind === SyntaxKind.InterfaceDeclaration) {
+          c.forEachChild(c1 => {
+            // if (c1.kind === SyntaxKind.Identifier) {
+            //   stateName = c1.getText()
+            // } else
+            if (c1.kind === SyntaxKind.PropertySignature) {
+              // stateProps.push({ [c1.getChildAt(0).getText()]: c1.getChildAt(2).getText() })
+              stateProps[c1.getChildAt(0).getText()] = c1.getChildAt(2).getText()
+            }
+          })
+        }
+      })
+
+      results[name] = stateProps
+    }
+
+    return results
+  }
+
+  context.storeAppActionList = async () => {
+    const {
+      filesystem: { inspectTree },
+    } = context
+
+    const tree = await inspectTree('src/store/actions')
+    if (!tree || !tree.children || !tree.children.length) {
+      return undefined
+    }
+
+    const files = tree.children
+      // @ts-ignore
+      .filter(r => r.type === 'file')
+      // @ts-ignore
+      .map(r => ({ name: r.name, path: 'src/store/actions/' + r.name }))
+
+    let results = {}
+    for (const file of files) {
+      const ast = fromPath(file.path)
+
+      ast.forEachChild(c => {
+        if (c.kind === SyntaxKind.FunctionDeclaration) {
+          let params = {}
+          let fnName = ''
+
+          c.forEachChild(c1 => {
+            if (c1.kind === SyntaxKind.Parameter) {
+              params[c1.getChildAt(0).getText()] = c1.getChildAt(2).getText()
+            } else if (c1.kind === SyntaxKind.Identifier) {
+              fnName = c1.getText()
+            }
+          })
+
+          results[fnName] = {
+            file: file.name,
+            params,
+          }
+        }
+      })
     }
 
     return results
