7f88013ecde23d23d48c8f995f021329599f1056
budiadiono <budiadiono@gmail.com>
2018-11-24 06:49:27 +0700

    change project description

--- src/commands/anoa.ts
+++ src/commands/anoa.ts
@@ -6,12 +6,12 @@ export default {
   run: async (context: RootContext) => {
     const { print, runtime } = context
 
-    print.success('          .d8888b. 88d888b. .d8888b. .d8888b. ')
-    print.success("          88'  `88 88'  `88 88'  `88 88'  `88 ")
-    print.success('          88.  .88 88    88 88.  .88 88.  .88 ')
-    print.success("           88888P8 dP    dP `88888P' `88888P8 ")
-    print.info('=========================================================')
-    print.info('  React Native Typescript Boilerplate & Scaffolder CLI')    
+    print.success('            .d8888b. 88d888b. .d8888b. .d8888b. ')
+    print.success("            88'  `88 88'  `88 88'  `88 88'  `88 ")
+    print.success('            88.  .88 88    88 88.  .88 88.  .88 ')
+    print.success("             88888P8 dP    dP `88888P' `88888P8 ")
+    print.info('============================================================')
+    print.info('  React Native Typescript Boilerplate & Code Generator CLI')    
     runtime.run('help')
   },
 }
