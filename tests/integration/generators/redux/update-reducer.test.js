69ba4c7f9b4c735146a274c82969e8b6a9a1b774
budiadiono <budiadiono@gmail.com>
2019-06-08 09:47:12 +0700

    add new action types to reducer test

--- tests/integration/generators/redux/update-reducer.test.js
+++ tests/integration/generators/redux/update-reducer.test.js
@@ -156,4 +156,110 @@ export const TaskReducer: Reducer<TaskState, TaskAction> = (
 `
     )
   })
+
+  test('should be able to add new action types to reducer', async () => {
+    await run(
+      ['s', 'a'],
+      [
+        // Select reducer
+        ENTER,
+
+        // Add new action type
+        'new action',
+        TAB,
+        'any',
+        ENTER,
+
+        // Add more action type
+        'setFoo',
+        TAB,
+        'number',
+        ENTER,
+
+        // stop add new action type
+        ENTER
+      ]
+    )
+
+    const { exists, cwd } = filesystem
+    const tsConfigFilePath = path.join(cwd(), 'tsconfig.json')
+    expect(exists(tsConfigFilePath)).toBeTruthy()
+
+    const project = new Project({
+      tsConfigFilePath
+    })
+
+    process.chdir('src/store/reducers/task')
+
+    // files are exists
+    expect(exists('index.ts')).toBeTruthy()
+    expect(exists('actions.ts')).toBeTruthy()
+    expect(exists('state.ts')).toBeTruthy()
+
+    const taskReducerFile = project.addExistingSourceFile('index.ts')
+    expect(taskReducerFile.getText()).toEqual(
+      `import { Reducer } from 'redux'
+import { TaskAction } from './actions'
+import { TaskState } from './state'
+
+export const TaskReducer: Reducer<TaskState, TaskAction> = (
+  state = {
+    state1: '',
+    state2: 0
+  },
+  action
+) => {
+  switch (action.type) {
+    case 'TASK/SET_STATE_1':
+      return { ...state, state1: action.payload }
+    case 'TASK/SET_STATE_2':
+      return { ...state, state2: action.payload }
+    case 'TASK/ANOTHER_ACTION':
+      return { ...state }
+    case 'TASK_REDUCER/NEW_ACTION':
+      return { ...state }
+    case 'TASK_REDUCER/SET_FOO':
+      return { ...state }
+    default:
+      return state
+  }
+}
+`
+    )
+
+    const taskActionsFile = project.addExistingSourceFile('actions.ts')
+    expect(taskActionsFile.getText()).toEqual(
+      `export type TaskAction =
+  | {
+      type: 'TASK/SET_STATE_1'
+      payload: string
+    }
+  | {
+      type: 'TASK/SET_STATE_2'
+      payload: number
+    }
+  | {
+      type: 'TASK/ANOTHER_ACTION'
+      payload: string
+    }
+  | {
+      type: 'TASK_REDUCER/NEW_ACTION'
+      payload: any
+    }
+  | {
+      type: 'TASK_REDUCER/SET_FOO'
+      payload: number
+    }
+`
+    )
+
+    const taskStateFile = project.addExistingSourceFile('state.ts')
+    expect(taskStateFile.getText()).toEqual(
+      `export interface TaskState {
+  state1: string
+  state2: number
+}
+`
+    )
+  })
 })
