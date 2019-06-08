de2006142aac04736fb7a837831e12225cae3587
budiadiono <budiadiono@gmail.com>
2019-06-08 10:24:52 +0700

    connect store to stateless component test

--- tests/integration/generators/redux/store-connect.test.js
+++ tests/integration/generators/redux/store-connect.test.js
@@ -1,5 +1,5 @@
 const tempy = require('tempy')
-const { run, ENTER, SPACE } = require('../../../runner')
+const { run, DOWN, ENTER, SPACE } = require('../../../runner')
 const path = require('path')
 const fs = require('fs-extra')
 const { filesystem } = require('gluegun')
@@ -114,6 +114,91 @@ export interface ComponentAStateProps {
 }
 
 export interface ComponentAActionProps {
+  commonSetStateTwo: (payload: number) => void
+}
+`
+    )
+  })
+
+  test('should be able to connect store to stateless component', async () => {
+    await run(
+      ['s', 'c'],
+      [
+        // Select states to map
+        SPACE,
+        ENTER,
+
+        // Select thunks to map
+        SPACE,
+        ENTER,
+
+        // Select view kind to connect
+        ENTER,
+
+        // Select a Component
+        DOWN,
+        ENTER
+      ]
+    )
+
+    const { exists, cwd } = filesystem
+    const tsConfigFilePath = path.join(cwd(), 'tsconfig.json')
+    expect(exists(tsConfigFilePath)).toBeTruthy()
+
+    const project = new Project({
+      tsConfigFilePath
+    })
+
+    process.chdir('src/views/components/component-b')
+
+    // files are exists
+    expect(exists('index.tsx')).toBeTruthy()
+    expect(exists('props.ts')).toBeTruthy()
+
+    const mainViewFile = project.addExistingSourceFile('index.tsx')
+    expect(mainViewFile.getText()).toEqual(
+      `import React from 'react'
+import { Text, View } from 'react-native'
+import { AppStore } from '../../../store'
+import { setStateTwoAction } from '../../../store/actions/common'
+import {
+  ComponentBActionProps,
+  ComponentBProps,
+  ComponentBStateProps
+} from './props'
+
+function _ComponentB(props: ComponentBProps) {
+  return (
+    <View>
+      <Text>ComponentB</Text>
+    </View>
+  )
+}
+
+export const ComponentB = AppStore.withStore<
+  ComponentBStateProps,
+  ComponentBActionProps
+>(
+  state => ({ taskState1: state.task.state1, taskState2: state.task.state2 }),
+  dispatch => ({
+    commonSetStateTwo: payload => dispatch(setStateTwoAction(payload))
+  })
+)(_ComponentB)
+`
+    )
+
+    const propsFile = project.addExistingSourceFile('props.ts')
+    expect(propsFile.getText()).toEqual(
+      `export interface ComponentBProps
+  extends Partial<ComponentBStateProps>,
+    Partial<ComponentBActionProps> {}
+
+export interface ComponentBStateProps {
+  taskState1: string
+  taskState2: number
+}
+
+export interface ComponentBActionProps {
   commonSetStateTwo: (payload: number) => void
 }
 `
